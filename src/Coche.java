04000ec707b168b67d82dec21c8279bf00018cab
bianqa <alumno24.nunez.bianca@ipm.edu.ar>
2023-03-21 09:04:48 -0300

    Ejercicios

--- src/Coche.java
+++ src/Coche.java
@@ -2,12 +2,60 @@ public class Coche {
     private String marca;
     private String modelo;
     private String color;
+    private int velocidad;
 
-    public Coche (String marca, String modelo, String color){
+    public Coche (String marca, String modelo, String color, int velocidad){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
+        this.velocidad = 0;
     }
 
+    public String getMarca() {
+        return marca;
+    }
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int acelerar(){
+        return velocidad++;
+    }
+
+    public int frenar(){
+        if (velocidad-- < 0){
+            return velocidad;
+        } else{
+            return velocidad--;
+        }
+    }
+
+    public int mostrar_velocidad(){
+        return velocidad;
+    }
 }
---
+++ src/Fecha.java
@@ -0,0 +1,96 @@
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha(){
+        this.dia = 1;
+        this.mes = 1;
+        this.anio = 1900;
+    }
+
+    public Fecha (int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean anio_bisciesto(){
+        return anio % 4 == 0;
+    }
+
+    public void valida(){
+        if (dia > 31){
+            dia = 1;
+        }
+        if (mes > 12){
+            mes =1;
+        }
+        if (anio > 2023){
+            anio = 1900;
+        }
+        if ( (mes==4 || mes==6 || mes==9 || mes==11) && dia > 30){
+            dia = 1;
+            mes = 1;
+        }
+        if (mes==2) {
+            if (anio_bisciesto() && dia > 29) {
+                dia = 1;
+                mes = 1;
+            } else if (!anio_bisciesto() && dia > 28) {
+                dia = 1;
+                mes = 1;
+            }
+        }
+    }
+
+    public int diasMes (int mes){
+        if (mes==1 ||  mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12){
+            return 31;
+        } else if(mes==4 || mes==6 || mes==9 || mes ==11){
+            return 30;
+        } else{
+            return 28;
+        }
+    }
+
+    public void corta(){
+        if (dia > 9 && mes > 9){
+            System.out.println(dia + "-" + mes + "-" + anio);
+        } else if (dia < 10 && mes < 10){
+            System.out.println("0" + dia + "-0" + mes + "-" + anio);
+        } else if (dia > 9){
+            System.out.println(dia + "-0" + mes + "-" + anio);
+        } else {
+            System.out.println("0" + dia + "-" + mes + "-" + anio);
+        }
+    }
+
+    public void largo(){
+    }
+
+}
